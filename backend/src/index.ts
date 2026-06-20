8a1957b1f6ea61b6a7ab0250be60e064e82af49d
Ricardo A. Sanjur A. <ricardosanjur@gmail.com>
2026-06-20 17:29:37 -0500

    feat: Connect frontend pages (Index & Organizacion) to SonicJS board-members collection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,12 +10,19 @@ import type { SonicJSConfig } from '@sonicjs-cms/core'
 // Import your collection configurations
 // Add new collections here after creating them in src/collections/
 import blogPostsCollection from './collections/blog-posts.collection'
+import boardMembersCollection from './collections/board-members.collection'
+import boxersCollection from './collections/boxers.collection'
+import eventsCollection from './collections/events.collection'
+import fightsCollection from './collections/fights.collection'
 
 // Register collections BEFORE creating the app
 // This ensures they are synced to the database on startup
 registerCollections([
   blogPostsCollection,
-  // Add more collections here as you create them
+  boardMembersCollection,
+  boxersCollection,
+  eventsCollection,
+  fightsCollection
 ])
 
 // Application configuration
